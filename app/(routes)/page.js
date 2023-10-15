ef801cac96d4bf2f15b3477aa810e3729160c569
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-15 23:21:07 +0530

    added gallary and info for product

--- app/(routes)/page.js
+++ app/(routes)/page.js
@@ -6,15 +6,15 @@ import Container from "@/components/ui/container";
 export const revalidate = 0;
 
 const HomePage = async () => {
-  const billboard = await getBillBoards("4f4046c2-25c1-4ca5-a52d-bcc66506e0a7");
-  const products = await getProducts({isFeatured : true});
+  const billboard = await getBillBoards("cc1c8d9d-eb06-4568-a67f-9683c5bdcc3f");
+  const products = await getProducts({ isFeatured: true });
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
-      </div>
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" items={products} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   );
