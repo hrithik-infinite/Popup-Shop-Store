b43b054079a7b784a4b521755f47daecaa538d2e
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-15 21:23:30 +0530

    added logo

--- components/navbar.js
+++ components/navbar.js
@@ -12,7 +12,7 @@ const NavBar = async () => {
       <Container>
         <div className="relative px-4 sm:px-4 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">Store</p>
+            <img src="/images/logo_black.png" className="object-contain h-40"></img>
           </Link>
           <MainNav data={categories} />
         </div>
