af20f36109097c53d6ee61c96b5b5c4ac11804db
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-15 22:28:04 +0530

    COMPLETED PRODUCT CARD UI

---
+++ components/ui/icon-button.js
@@ -0,0 +1,10 @@
+"use client";
+import { cn } from "@/lib/utils";
+
+const IconButton = ({ onClick, className, icon }) => {
+  return <button onClick={onClick} className={cn("rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition", className)} >
+    {icon}
+  </button>;
+};
+
+export default IconButton;
