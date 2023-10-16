a6371fc37f36e86c56a467be0b03dd0c707dc860
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-16 21:56:15 +0530

    removed extra console log

--- actions/get-category.js
+++ actions/get-category.js
@@ -1,7 +1,6 @@
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 const getCategory = async (id) => {
-  console.log(id);
   const res = await fetch(`${URL}/${id}`);
 
   return res.json();
