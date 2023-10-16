4570a5da927ad939f798aac131c2282b39a5eb6b
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-16 20:59:06 +0530

    final touches

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { Urbanist } from "next/font/google";
 import Footer from "@/components/footer";
 import NavBar from "@/components/navbar";
 import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 const font = Urbanist({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={font.className}>
         <ModalProvider />
+        <ToastProvider />
         <NavBar />
         {children}
         <Footer />
