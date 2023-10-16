957cf8fcb53cd5663e4b6ee82489f624b786b96b
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-16 21:22:15 +0530

    trigger build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ["res.cloudinary.com"],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
+
+module.exports = nextConfig;
